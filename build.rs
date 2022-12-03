34bd37d275d80abf2ecc56f1e398bab67e98286e
DRAGHETTO\Simone <simo.anfuso.sa@gmail.com>
2022-12-03 11:04:34 +0100

    percorsi

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-lib=static=wpcap");
     println!("cargo:rustc-link-search=native=C:/Users/lucio/Desktop/rustProject/SDK_npcap/Lib/x64");
+    //println!("cargo:rustc-link-search=native=C:/Users/Simone/Desktop/PDS/npcap-sdk-1.13/Lib/x64");
+    //println!("cargo:rustc-link-search=native=C:/Users/Utente/OneDrive - Politecnico di Bari/Da spostare/POLITECNICO DI TORINO/+-- PROGRAMMAZIONE DI SISTEMA 7,75 +/npcap-sdk-1.13/Lib/x64");
 }
